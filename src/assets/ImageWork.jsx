e0da6ecb7d1c53db7d145ff84b4545b20e44da17
nayarakarine.araujo@gmail.com <nayarakarine.araujo@gmail.com>
2024-10-04 15:03:15 -0300

    Colocando Imagens no projeto

---
+++ src/assets/ImageWork.jsx
@@ -0,0 +1,22 @@
+import Piscineiro from './imageWork/Front-piscineiro.png';
+import Tecnico from './imageWork/itamarMaquinas.png';
+import SpellBook from './imageWork/spellBook.png';
+import CalculadoraImc from './imageWork/CalculadoraIMC.png';
+import TodoList from './imageWork/TodoListReact.png';
+import JogoNSecreto from './imageWork/jogo-n-secreto.png';
+import Decodificador from './imageWork/decodificador.png';
+import ConversorMoedas from './imageWork/'
+import PortalTech from './imageWork/img-portaltech.jpg';
+
+const ImagesWork = {
+    "front-piscinas": Piscineiro, 
+    "front-tecnico": Tecnico,
+    "front-dndSpellApp": SpellBook,
+    "front-imcCalculator": CalculadoraImc,
+    "front-taskList": TodoList,
+    "front-numeroSecreto": JogoNSecreto,
+    "front-decodificadorTexto": Decodificador,
+    "fullstack-sistemaChamados": PortalTech,
+};
+
+export default ImagesWork
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import projectsData from "../ProjectsData.json";
+import ImagesWork from "../assets/ImageWork";
 import styles from "./Projects.module.css";
 
 export default function Projects() {
-  const projectsFront = projectsData.projects.front; 
+  const projectsFront = projectsData.projects.front;
   const projectsBack = projectsData.projects.back;
   const projectsFullstack = projectsData.projects.fullstack;
 
@@ -17,10 +18,23 @@ export default function Projects() {
           {projectsFront.map((project) => (
             <div key={project.id} className={styles.project_card}>
               <h3>{project.title}</h3>
+
+              <div>
+                <img
+                  src={ImagesWork[project.id] || "default-image-path.jpg"}
+                  alt={project.title} 
+                  className="product-image"
+                />
+              </div>
+
               <p>{project.description}</p>
               <div className={styles.project_links}>
                 {project.links.project && (
-                  <a href={project.links.project} target="_blank" rel="noopener noreferrer">
+                  <a
+                    href={project.links.project}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
                     Ver Projeto
                   </a>
                 )}
@@ -28,7 +42,11 @@ export default function Projects() {
                   <span> | </span>
                 )}
                 {project.links.repository && (
-                  <a href={project.links.repository} target="_blank" rel="noopener noreferrer">
+                  <a
+                    href={project.links.repository}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
                     Ver Repositório
                   </a>
                 )}
@@ -38,6 +56,52 @@ export default function Projects() {
         </div>
       </section>
 
+
+      <section id="fullstack">
+        <h2>Projetos de FullStack</h2>
+        <div className={styles.projects_grid}>
+          {projectsFullstack.map((project) => (
+            <div key={project.id} className={styles.project_card}>
+              <h3>{project.title}</h3>
+
+              <div>
+                <img
+                  src={ImagesWork[project.id] || "default-image-path.jpg"}
+                  alt={project.title}
+                  className="product-image"
+                />
+              </div>
+
+              <p>{project.description}</p>
+              <div className={styles.project_links}>
+                {project.links.project && (
+                  <a
+                    href={project.links.project}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Ver Projeto
+                  </a>
+                )}
+                {project.links.project && project.links.repository && (
+                  <span> | </span>
+                )}
+                {project.links.repository && (
+                  <a
+                    href={project.links.repository}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Ver Repositório
+                  </a>
+                )}
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
+
+      
       <section id="back">
         <h2>Projetos de Back-end</h2>
         <div className={styles.projects_grid}>
@@ -47,7 +111,11 @@ export default function Projects() {
               <p>{project.description}</p>
               <div className={styles.project_links}>
                 {project.links.project && (
-                  <a href={project.links.project} target="_blank" rel="noopener noreferrer">
+                  <a
+                    href={project.links.project}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
                     Ver Projeto
                   </a>
                 )}
@@ -55,7 +123,11 @@ export default function Projects() {
                   <span> | </span>
                 )}
                 {project.links.repository && (
-                  <a href={project.links.repository} target="_blank" rel="noopener noreferrer">
+                  <a
+                    href={project.links.repository}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
                     Ver Repositório
                   </a>
                 )}
@@ -65,32 +137,6 @@ export default function Projects() {
         </div>
       </section>
 
-      <section id="fullstack">
-        <h2>Projetos de FullStack</h2>
-        <div className={styles.projects_grid}>
-          {projectsFullstack.map((project) => (
-            <div key={project.id} className={styles.project_card}>
-              <h3>{project.title}</h3>
-              <p>{project.description}</p>
-              <div className={styles.project_links}>
-                {project.links.project && (
-                  <a href={project.links.project} target="_blank" rel="noopener noreferrer">
-                    Ver Projeto
-                  </a>
-                )}
-                {project.links.project && project.links.repository && (
-                  <span> | </span>
-                )}
-                {project.links.repository && (
-                  <a href={project.links.repository} target="_blank" rel="noopener noreferrer">
-                    Ver Repositório
-                  </a>
-                )}
-              </div>
-            </div>
-          ))}
-        </div>
-      </section>
     </div>
   );
 }
