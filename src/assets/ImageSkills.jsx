076eedf423bc8f98360707c38c3ec5edc0066fc0
Nayara Karine <nayarakarine.araujo@gmail.com>
2026-03-30 19:39:22 -0300

    refactor: ( imageSkills )
    - Removido foto de logo de skills que não estavam mais sendo usadas;

--- src/assets/ImageSkills.jsx
+++ src/assets/ImageSkills.jsx
@@ -5,13 +5,11 @@ import Figma from "../assets/imageSkills/figma-logo.png";
 import Git from "../assets/imageSkills/git-logo.png";
 import GitHub from "../assets/imageSkills/github-logo.png";
 import HTML5 from "../assets/imageSkills/html5-logo.png";
-import Java from "../assets/imageSkills/java-logo.png";
 import JavaScript from "../assets/imageSkills/javascript-logo.png";
 import TypeScript from "../assets/imageSkills/typescript-logo.png";
 import MySql from "../assets/imageSkills/mysql-logo.png";
 import Postman from "../assets/imageSkills/postman-logo.png";
 import React from "../assets/imageSkills/react-logo.png";
-import SpringBoot from "../assets/imageSkills/springBoot-logo.png";
 import VueJs from "../assets/imageSkills/vueJs-logo.webp";
 import Mui from "../assets/imageSkills/mui-logo.png";
 import Antdesign from "../assets/imageSkills/antdesign-logo.png";
@@ -23,13 +21,11 @@ const Images = {
   git: Git,
   github: GitHub,
   html5: HTML5,
-  java: Java,
   javascript: JavaScript,
   typescript: TypeScript,
   mysql: MySql,
   postman: Postman,
   react: React,
-  springBoot: SpringBoot,
   vuejs: VueJs,
   mui: Mui,
   antdesign: Antdesign,
