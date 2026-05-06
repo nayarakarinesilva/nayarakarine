8deb5a441bf0255abd12485553483046c9da9a28
Nayara Karine <nayarakarine.araujo@gmail.com>
2026-05-06 19:49:52 -0300

    refactor: ( Navbar )
    - Feitos ajustes inicias para atualização do portfólio

--- src/components/Projects/ProjectsSections.jsx
+++ src/components/Projects/ProjectsSections.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import projectsData from "../../ProjectsData.json";
+import projectsData from "../../mock/ProjectsData.json";
 import ImagesWork from "../../assets/ImageWork";
 import styles from "./Projects.module.css";
 
